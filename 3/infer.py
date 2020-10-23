5181510806b8bb3a2cfe00fab552281df2a58af9
ninetailskim <ninetailsyeon@163.com>
2020-10-23 19:33:55 +0800

    1933

--- 3/infer.py
+++ 3/infer.py
@@ -1,4 +1,19 @@
+import cv2
+import os
+import paddle
+import paddle.fluid as fluid
+import numpy as np
+import argparse
+from basic_model import BasicModel
+from basic_data_preprocessing import InferAugmentation
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--imagelist',type=str, default="")
+parser.add_argument('--modelpath', type=str, default=0)
+parser.add_argument('--image_folder', type=str, required=True)
+
+args = parser.parse_args()
 
 def colorize(gray, palette):
     # gray: numpy array of the label and 1*3N size list palette
@@ -17,14 +32,29 @@ def save_blend_image(image_file, pred_file):
     image.save(o_file)
 
 
-
-
 def inference_resize()
+
+
 
 def inference_sliding()
 
-def inference_multi_scale()
 
+
+def inference_multi_scale()
+    for scale in scales:
+        image = ReScale(image)
+        logits = Model(image)
+        if flip:
+            image_flip = Flip(image)
+            logits_flip = Model(image_flip)
+        
+        pred.append(ScaleBack(logits))
+        if flip:
+            pred.append(ScaleBack(Flip(logits_flip)))
+
+    pred = mean(pred)
+    score = softmax(pred)
+    pred = argmax(pred)
 
 
 def save_images
@@ -32,26 +62,54 @@ def save_images
 
 
 # this inference code reads a list of image path, and do prediction for each image one by one
-def main():
+def main(args):
     # 0. env preparation
+    with fluid.dygraph.guard(fluid.CPUPlace()):
+        # 1. create model
+        model = BasicModel()
+        model.eval()
+        # 2. load pretrained model 
+        params_dict, _ = fluid.load_dygraph(args.model_path)
+        model.load_dict(params_dict)
+        # 3. read test image list
+        data_list = []
+        with open(args.imagelist, 'r') as infile:
+            for line in infile:
+                data_path = os.path.join(args.image_folder, line.split()[0])
+                label_path = os.path.join(args.image_folder, line.split()[1])
+                data_list.append((data_path, label_path))
 
-    # 1. create model
+        # 4. create transforms for test image, transform should be same as training
+        transforms = InferAugmentation()
 
-    # 2. load pretrained model 
+        color_list = []
+        with open('pascal_context_colors.txt', 'r') as colorfile:
+            for line in infile:
+                ll = line.split()
+                color_list.append(int(ll[0]))
+                color_list.append(int(ll[1]))
+                color_list.append(int(ll[2]))
 
-    # 3. read test image list
+        # 5. loop over list of images
+        for index, data in enumerate(data_list):
+            # 6. read image and do preprocessing
+            
+            image = cv2.imread(data[0], cv2.IMREAD_COLOR)
+            image = cv2.cvtColor(data, cv2.COLOR)BGR2RGB
+            image = image[np.newaxis,:,:,:]
+            # 7. image to variable
+            image = fluid.dygraph.to_variable(image)
+            image = fluid.layers.transpose(image, [0, 3, 1, 2])
+            # 8. call inference func
+            image = model(image)
+            # 9. save results
+            image = fluid.layers.squeeze(image)
 
-    # 4. create transforms for test image, transform should be same as training
+            pred = colorize(image.numpy() ,color_list)
 
-    # 5. loop over list of images
+            cv2.imwrite(str(index)+ '_.png', pred)
 
-        # 6. read image and do preprocessing
-
-        # 7. image to variable
-
-        # 8. call inference func
-
-        # 9. save results
+            save_blend_image(data[0], 'tmp.png')
 
 if __name__ == "__main__":
-    main()
+    main(args)
